6980a78dbe159f588682d7c1de216524d1b50041
Hiro Kato <hirokikato1@gmail.com>
2023-07-19 09:53:30 -0400

    builds simple liker app

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//invoke mimicServerCall in response to user action
+//function randomly returns "success" or "fail"
+//if successful update the heart to toggle between empty and full
+//if fail display the error messages to the DOM
 
+//mimicServerCall()
+const modal = document.getElementById("modal").setAttribute('class', 'hidden')
+const likeGlyphs = document.getElementsByClassName("like-glyph")
+
+for(let heart in likeGlyphs){
+  likeGlyphs[heart].addEventListener("click", (event) => {
+    const heart = event.target;
+    mimicServerCall()
+    .then(res => {
+      if(heart.innerText === FULL_HEART){
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove("activated-heart")
+      }else{
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart");
+      }
+    })
+    .catch((err) => {
+      document.getElementById("modal").classList.remove("hidden")
+      document.getElementById("modal-message").textContent = err
+      setTimeout(()=> {document.getElementById("modal").setAttribute('class', 'hidden')}, 3000)
+    })
+  })
+}
 
 
 
